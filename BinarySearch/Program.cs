931a936ad48567a687d1120b5a18e6e732bd0b63
spg <sperisgimeno@gmail.com>
2023-12-07 21:10:37 +0100

    07.12 - Added example for showing how binary search works.

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinarySearchAlgorithm;
 
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("This is a demo of a binary search algorithm:");
+
+        List<int> sortedNumbers = new List<int>() { 1,3,5,7,9,11 };
+        int numberToSearch = 9;
+
+        var binarySearch = new BinarySearch(sortedNumbers, numberToSearch);
+
+        var foundNumberIndex = binarySearch.Operate();
+
+        Console.WriteLine("Found number index: " + foundNumberIndex);
+        Console.ReadKey();
+    }
+}
