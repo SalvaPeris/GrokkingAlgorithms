53968abf6df87b25ac11c22e5757958bc54da561
spg <sperisgimeno@gmail.com>
2023-12-07 21:08:59 +0100

    07.12 - Added BinarySearch algorithm.

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿namespace BinarySearchAlgorithm
+{
+	public class BinarySearch
+	{
+		private int low;
+		private int high;
+		private int mid;
+		private int guess;
+		private List<int> _sortedNumbers;
+		private int _numberToSearch;
+		private int numberOfOperations;
+
+		public BinarySearch(List<int> sortedNumbers, int numberToSearch)
+		{
+			numberOfOperations = 0;
+			low = 0;
+			high = sortedNumbers.Count - 1;
+			_sortedNumbers = sortedNumbers;
+			_numberToSearch = numberToSearch;
+		}
+
+		public int? Operate()
+		{
+			while (low <= high)
+			{
+				mid = (low + high) / 2;
+				guess = _sortedNumbers[mid];
+
+				if (guess == _numberToSearch)
+				{
+					Console.WriteLine("Number of operations : " + numberOfOperations);
+					return mid;
+                }
+				else if (guess > _numberToSearch)
+					high = mid - 1;
+				else
+					low = mid + 1;
+
+				numberOfOperations++;
+			}
+
+			return null;
+		}
+	}
+}
+
