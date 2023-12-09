da0f01eb86a790be8eeb1ed2310f25be4967377b
speris <speris@grupokh.com>
2023-12-09 14:18:23 +0100

    09.12 - Added FindSmallest class.

---
+++ SelectionSort/FindSmallest.cs
@@ -0,0 +1,21 @@
+﻿namespace SelectionSort
+{
+    public static class FindAlgorithms
+    {
+        public static int FindSmallest(int[] numbers)
+        {
+            int smallest = numbers[0];
+            int smallestIndex = 0;
+            
+            foreach (var i in numbers) {
+                if(i < smallest)
+                {
+                    smallest = numbers[i];
+                    smallestIndex = i;
+                }
+            }
+
+            return smallestIndex;
+        }
+    }
+}
