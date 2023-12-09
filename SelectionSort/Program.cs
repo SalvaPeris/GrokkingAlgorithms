60ffa524aa79b2c71ca26c5ddb09e15e3e5be5d5
speris <speris@grupokh.com>
2023-12-09 15:28:57 +0100

    09.12 - Added SelectionSort example.

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using SelectionSortAlgorithms;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is a SelectionSort demo:");
+        int[] numbers = { 3, 6, 2, 5, 4, 1, 9, 8, 7 };
+
+        var sortedNumbers = new SelectionSort(numbers).Sort();
+
+        Console.WriteLine("Unsorted numbers: ");
+        numbers.ToList().ForEach(x => Console.WriteLine(x));
+
+        Console.WriteLine("Sorted numbers: " );
+        sortedNumbers.ToList().ForEach(x => Console.WriteLine(x));
+        Console.ReadKey();
     }
 }
