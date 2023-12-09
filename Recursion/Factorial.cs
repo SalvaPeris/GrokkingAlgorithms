f70b00e30f5d63deb238d10c57966c1cc4bbc080
speris <speris@grupokh.com>
2023-12-09 15:58:56 +0100

    09.12 - Added Recursion class for getting a factorial of a number demo.

---
+++ Recursion/Factorial.cs
@@ -0,0 +1,12 @@
+﻿namespace Recursion
+{
+    public static class RecursionMethods
+    {
+        public static int Factorial(int number)
+        {
+            if(number == 1)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,14 @@
+﻿using Recursion;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("This is a recursion class demo for get the factorial of a number: ");
+        var number = 5;
+        var factorial = RecursionMethods.Factorial(number);
+
+        Console.WriteLine($"Factorial of number {number}: " + factorial);
+        Console.ReadKey();
+    }
+}
