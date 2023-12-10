d699fae0373051a02828b06ed756e9fcb5cd77df
speris <speris@grupokh.com>
2023-12-10 19:58:08 +0100

    10.12 - Added QuickSort algorithm and an example to prove it.

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using QuickSortAlgorithm;
+
+internal class Program
 {
     private static void Main()
     {
         Console.WriteLine("This is a quick sort algorithm demo :");
+
+        int[] numbers = new int[] { 5, 1, 4, 6, 3, 9, 7, 2, 8 };
+
+        var result = Sort.QuickSort(numbers);
+
+        Console.WriteLine("Unsorted array: ");
+        numbers.ToList().ForEach(x => Console.WriteLine(x));
+
+        Console.WriteLine("Sorted array: ");
+        result.ToList().ForEach(x => Console.WriteLine(x));
+        Console.ReadKey();
     }
 }
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,23 @@
+﻿namespace QuickSortAlgorithm
+{
+    public static class Sort
+    {
+        public static int[] QuickSort(int[] numbers)
+        {
+            if(numbers.Length < 2)
+                return numbers;
+            else
+            {
+                var pivot = numbers[0];
+                var less = numbers.Where(x => x < pivot).ToArray();
+                var greater = numbers.Where(x => x > pivot).ToArray();
+
+                var result = QuickSort(less).ToList();
+                result.Add(pivot);
+                result.AddRange(QuickSort(greater));
+
+                return result.ToArray();
+            }
+        }
+    }
+}
